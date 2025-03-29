870d87c8e16dd679e821c91666f47da3969893fc
bauti05 <bperez@mail.austral.edu.ar>
2025-03-29 19:36:30 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    name=first_name+" "+last_name
+    print(name.lower())
+    print(name.title())
+    print(name.upper())
+    name_sangria= f"\t{name}"
+    print(name_sangria.lower())
+ada()
